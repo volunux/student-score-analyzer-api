ff87494266a0359cb76c1d66d37391ccda999b87
Yusuf Musa <volunux@gmail.com>
2022-07-28 21:59:45 +0100

    Set origin url

--- src/main/java/com/tplnt/studentscore/controller/HomeController.java
+++ src/main/java/com/tplnt/studentscore/controller/HomeController.java
@@ -1,13 +1,11 @@
 package com.tplnt.studentscore.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tplnt.studentscore.entity.StudentAnalyzer;
 
-@RestController
-@RequestMapping("/")
+@RestController("")
 public class HomeController {
 
 	@GetMapping
