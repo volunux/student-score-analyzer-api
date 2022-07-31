7d289958325316bfafe782b33df17ffc0d77025c
Yusuf Musa <volunux@gmail.com>
2022-08-01 00:00:50 +0100

    Artifact ID update

--- src/test/java/com/tplnt/subject/DemoApplicationTests.java
+++ src/test/java/com/tplnt/subject/DemoApplicationTests.java
@@ -1,12 +1,8 @@
 package com.tplnt.subject;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class DemoApplicationTests {
 
-	@Test
 	void contextLoads() {
 	}
 
